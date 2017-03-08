082080f35d486182d858cfed9e86a321c4298935
github username <github email address>
2017-03-07 19:45:12 -0500

    mm

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'application#index'
+  post '/products/add', to: 'products#add', as: "add"
+
+  resources :products, only: [:index]
 end
